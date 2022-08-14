2761febcffe1c6cf476abfea7b9743868ca8cca6
roger.nomen.sge <roger.nomen@championgames.com>
2022-08-14 10:10:38 +0200

    New Login google

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  function handleCredentialResponse(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  const [gsiScriptLoaded, setGsiScriptLoaded] = useState(false)
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    if (user?._id || gsiScriptLoaded) return
+
+    const initializeGsi = () => {
+      // Typescript will complain about window.google
+      // Add types to your `react-app-env.d.ts` or //@ts-ignore it.
+      if (!window.google || gsiScriptLoaded) return
+
+      setGsiScriptLoaded(true)
+      window.google.accounts.id.initialize({
+        client_id: "907100418669-pa4c1oeginl8k39430qsv4gjgpdp3jmi.apps.googleusercontent.com",
+        callback: handleGoogleSignIn,
+      })
+    }
+
+    const script = document.createElement("script")
+    script.src = "https://accounts.google.com/gsi/client"
+    script.onload = initializeGsi
+    script.async = true
+    script.id = "google-client-script"
+    document.querySelector("body")?.appendChild(script)
+
+    return () => {
+      // Cleanup function that runs when component unmounts
+      window.google?.accounts.id.cancel()
+      document.getElementById("google-client-script")?.remove()
+    }
+  }, []);
+
+  const handleGoogleSignIn = (res) => {
+    if (!res.clientId || !res.credential) return
+
+    console.log("respones", res);
+    // Implement your login mutations and logic here.
+    // Set cookies, call your backend, etc.
   }
 
   return (
@@ -25,7 +62,7 @@ function App() {
              data-client_id="907100418669-pa4c1oeginl8k39430qsv4gjgpdp3jmi.apps.googleusercontent.com"
              data-callback="handleCredentialResponse">
         </div>
-        <div className="g_id_signin" data-type="standard"></div>
+        <div className={"g_id_signin"} />
       </header>
     </div>
   );
